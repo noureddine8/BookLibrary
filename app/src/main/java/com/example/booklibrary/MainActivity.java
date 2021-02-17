a35d58ca9c954a140843d15b454bc445c6ba690a
Louafi Noureddine <noureddinelouafi8@gmail.com>
2021-02-17 14:16:59 +0100

    First page - all books page

--- app/src/main/java/com/example/booklibrary/MainActivity.java
+++ app/src/main/java/com/example/booklibrary/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.booklibrary;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button allBooksBtn,currentBtn,alreadyBtn,favouriteBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initViews();
+        allBooksBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,AllBooks.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void initViews() {
+        allBooksBtn = findViewById(R.id.firstBtnId);
+        currentBtn = findViewById(R.id.secondBtnId);
+        alreadyBtn = findViewById(R.id.thirdBtnId);
+        favouriteBtn = findViewById(R.id.fourthBtnId);
+
     }
 }
